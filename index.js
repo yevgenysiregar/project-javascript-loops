ea2c288033fb14c85adf70effe98e6643459d69e
Yevgeny Omar Siregar <siregar.yevgeny@gmail.com>
2019-04-11 14:58:49 +0700

    Add code on index.js

--- index.js
+++ index.js
@@ -4,3 +4,21 @@ while (i) {
   console.log(i);
   i--;
 }
+
+let x = 5;
+
+do {
+  console.log(x);
+  x += 5;
+} while (x < 20);
+
+const age = [10, 16, 17, 19, 23, 25, 40, 60, 73];
+
+for (let index = 0; index < age.length; index++) {
+  const numbers = age[index];
+  console.log(numbers);
+}
+
+for (let y = 50; y > 0; y -= 10) {
+  console.log(y);
+}
